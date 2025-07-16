c75ba7f7510b3afb2ed2c5dc57311fdc17682089
joaovx1315 <joaovitorvx1315@gmail.com>
2025-07-16 16:21:27 -0300

    monorepo

--- frontend/web/vite.config.ts
+++ frontend/web/vite.config.ts
@@ -4,4 +4,10 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173, 
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+  },
 })
