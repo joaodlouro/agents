e9f8d559580da76c54a57838903035c29ed72f58
joaovx1315 <joaovitorvx1315@gmail.com>
2025-07-16 01:01:36 -0300

    rooms listing

--- src/http/roules/get-rooms.ts
+++ src/http/roules/get-rooms.ts
@@ -1,8 +1,18 @@
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod';
+import { db } from '../../db/connection.ts';
+import { schema } from '../../db/schema/index.ts'; 
 
-export const getRoomsRoute: FastifyPluginCallbackZod =  (app) => {
+export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
+  app.get('/rooms', async () => {
+    const result = await db
+      .select({
+        id: schema.rooms.id,
+        name: schema.rooms.name,
+        
+      })
+      .from(schema.rooms)
+     .orderBy(schema.rooms.createdAT)
 
-  app.get('/rooms',() => {
-    return'hello world';
-  })
-}
+    return result; 
+  });
+};
